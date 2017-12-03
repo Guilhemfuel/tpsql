403005dcf802376994b4359c2c2159331a990282
Guilhem <guilhem.canivet@gmail.com>
2017-12-03 18:49:00 +0100

    Maj Sql

--- script.php
+++ script.php
@@ -2,7 +2,7 @@
 
 try
 {
-	$bdd = new PDO('mysql:host=localhost:8889;charset=utf8', 'root', 'root');
+	$bdd = new PDO('mysql:host=localhost;charset=utf8', 'root', '');
 }
 catch (Exception $e)
 {
@@ -38,7 +38,7 @@ $bdd->exec('CREATE TABLE `shop`.produits (
 $bdd->exec('CREATE TABLE `shop`.commandes (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     client INT NOT NULL,
-    date DATETIME NOT NULL,
+    date_commande DATETIME NOT NULL,
     CONSTRAINT fk_client FOREIGN KEY commandes(client) REFERENCES shop.clients(id)
 )');
 
@@ -74,15 +74,15 @@ $tags = array(
 $tags = json_encode($tags);
 $tags = json_decode($tags);
 
-$req = $bdd->prepare('INSERT INTO tags VALUES(:id, :nom)');
+$req = $bdd->prepare('INSERT INTO tags (nom) VALUES(:nom)');
 
 foreach($tags as $data)
 {
 	$req->execute(array(
-	'id' => '',
-	'nom' => $data->nom
+	'nom' => $data->nom,
 	));
 }
+
 
 $produits = array(
 	array("nom" => "PS4", "description" => "Console de jeu", "quantite" => 4,
@@ -101,7 +101,7 @@ $produits = array(
 
 $clients = array(
 	array("nom" => "Canivet", "prenom" => "Guilhem", "email" => "guilhem@gmail.com",
-		"phones" => array('0671172758', '0548451521', '0687484545', '0587484545')
+		"phones" => array('0671172758', '0548451521', '0687484545')
 	),
 	array("nom" => "Dupuis", "prenom" => "Jean", "email" => "jean@gmail.com",
 		"phones" => array('0548784554')
@@ -117,18 +117,17 @@ $produits = json_decode($produits);
 $clients = json_encode($clients);
 $clients = json_decode($clients);
 
-$req = $bdd->prepare('INSERT INTO produits VALUES(:id, :nom, :description, :quantite)');
+$req = $bdd->prepare('INSERT INTO produits (nom, description, quantite) VALUES(:nom, :description, :quantite)');
 $req2 = $bdd->prepare('INSERT INTO produit_tag VALUES(:produit, :tag)');
 
 foreach($produits as $data)
 {
 	$req->execute(array(
-	'id' => '',
 	'nom' => $data->nom,
 	'description' => $data->description,
-	'quantite' => $data->quantite
+	'quantite' => $data->quantite,
 	));
-
+	
 	$id = $bdd->lastInsertId();
 
 	foreach($data->tags as $data)
@@ -140,13 +139,13 @@ foreach($produits as $data)
 	}
 }
 
-$req = $bdd->prepare('INSERT INTO clients (id, nom, prenom, email) VALUES(:id, :nom, :prenom, :email)');
-$req2 = $bdd->prepare('INSERT INTO phones VALUES(:id, :numero, :client)');
+$req = $bdd->prepare('INSERT INTO clients (nom, prenom, email) VALUES(:nom, :prenom, :email)');
+$req2 = $bdd->prepare('INSERT INTO phones (numero, client) VALUES(:numero, :client)');
 
 foreach($clients as $data)
 {
+	
 	$req->execute(array(
-	'id' => '',
 	'nom' => $data->nom,
 	'prenom' => $data->prenom,
 	'email' => $data->email
@@ -157,7 +156,6 @@ foreach($clients as $data)
 	foreach($data->phones as $data)
 	{
 		$req2->execute(array(
-			'id' => '',
 			'numero' => $data,
 			'client' => $id,
 		));
@@ -176,13 +174,12 @@ $commandes = array(
 $commandes = json_encode($commandes);
 $commandes = json_decode($commandes);
 
-$req = $bdd->prepare('INSERT INTO commandes VALUES(:id, :client, :date)');
-$req2 = $bdd->prepare('INSERT INTO commande_produit VALUES(:id, :commande, :produit)');
+$req = $bdd->prepare('INSERT INTO commandes (client, date_commande) VALUES(:client, :date)');
+$req2 = $bdd->prepare('INSERT INTO commande_produit (commande, produit) VALUES(:commande, :produit)');
 
 foreach($commandes as $data)
 {
 	$req->execute(array(
-	'id' => '',
 	'client' => $data->client,
 	'date' => $data->date
 	));
@@ -192,7 +189,6 @@ foreach($commandes as $data)
 	foreach($data->produits as $data)
 	{
 		$req2->execute(array(
-			'id' => '',
 			'commande' => $id,
 			'produit' => $data,
 		));
@@ -202,15 +198,64 @@ foreach($commandes as $data)
 //Rename signin_at
 $bdd->exec('ALTER TABLE `shop`.clients CHANGE signin_at signup_at DATETIME DEFAULT CURRENT_TIMESTAMP');
 
-$response = $bdd->query('SELECT produits.nom, COUNT(produit_tag.tag) as nb_tag 
+
+//Compter le nombre de tags par produit
+$response = $bdd->query('SELECT produits.id, produits.nom, COUNT(produit_tag.tag) as nb_tag 
 	FROM produits 
 	LEFT JOIN produit_tag ON produit_tag.produit = produits.id 
-	GROUP BY nom
+	GROUP BY id
 	ORDER BY nom ASC');
 
 while ($donnees = $response->fetch())
 {
 	echo 'Produit : ' . $donnees['nom'] .' - Nombre de tag : '.$donnees['nb_tag']. '<br />';
+	echo 'Tags : ';
+	
+	//Afficher les noms des tags
+	$response2 = $bdd->query('SELECT tags.nom FROM tags, produit_tag WHERE produit_tag.produit = '.$donnees['id'].' AND produit_tag.tag = tags.id');
+	while ($donnees2 = $response2->fetch())
+	{
+		echo $donnees2['nom'].' - ';
+	}
+	
+	echo '<br /><br />';
+}
+
+echo 'Combien de fois chaque tag a été utilisé : <br />';
+//Liste des tags et combien de fois est utilisé chaque tag
+$response = $bdd->query('SELECT tags.nom, COUNT(produit_tag.tag) as nb FROM tags LEFT JOIN produit_tag ON tags.id = produit_tag.tag GROUP BY tags.nom');
+while ($donnees = $response->fetch())
+{
+	echo $donnees['nom'].' - '.$donnees['nb'];
+	echo '<br />';
+}
+
+echo '<br />';
+echo 'Tag le plus lié : ';
+$response = $bdd->query('SELECT tags.nom, COUNT(produit_tag.tag) as nb FROM tags LEFT JOIN produit_tag ON tags.id = produit_tag.tag GROUP BY tags.nom ORDER BY nb DESC LIMIT 1');
+while ($donnees = $response->fetch())
+{
+	echo $donnees['nom'].' - '.$donnees['nb'];
+	echo '<br />';
+}
+
+echo '<br />';
+echo 'Les produits qui ont des tags : <br />';
+$response = $bdd->query('SELECT produits.nom FROM produits WHERE produits.id IN (SELECT produit_tag.produit FROM produit_tag)');
+while ($donnees = $response->fetch())
+{
+	echo $donnees['nom'];
+	echo '<br />';
+}
+
+echo '<br />';
+echo 'Les produits qui ont au moins 2 tags: <br />';
+$response = $bdd->query('SELECT DISTINCT(produits.nom), COUNT(nom) as nb FROM produits WHERE produits.id IN (SELECT produit FROM produit_tag GROUP BY produit) GROUP BY nom');
+while ($donnees = $response->fetch())
+{
+	echo $donnees['nom'];
+	echo $donnees['nb'];
+	echo '<br />';
 }
 
 ?>
